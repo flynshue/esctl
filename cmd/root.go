e64c9a575f7d3c2a45be909ad06e5c760ff7e941
Felicia Lyn-Shue <flynshue@gmail.com>
2023-09-15 11:33:33 -0400

    start auto generating docs for every command

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 	"github.com/spf13/viper"
 )
 
@@ -24,6 +25,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	doc.GenMarkdownTree(rootCmd, "docs")
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
